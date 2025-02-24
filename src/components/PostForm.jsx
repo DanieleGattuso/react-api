cec5ecc536adce448a7c3ab2e0f1d691af215216
daniele.gattuso <d.gattuso09@gmail.com>
2025-02-24 09:52:44 +0100

    setting everything

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -10,3 +10,24 @@ const initialFormData = {
     tags: [],  // Array di tag separati da virgole
     // published: false, // Opzione per indicare se il post è pubblicato (attualmente commentata)
 };
+
+const PostForm = () => {
+
+    // Stato per memorizzare la lista dei post esistenti
+    const [posts, setPosts] = useState([]);
+
+    // Stato per gestire i dati inseriti nel form
+    const [formData, setFormData] = useState(initialFormData);
+
+    // Funzione per recuperare i post dal server tramite una richiesta GET
+    function fetchPosts() {
+        axios.get("http://localhost:3000/posts")
+            .then((res) => {
+                // Aggiorniamo lo stato con i dati ricevuti dall'API
+                setPosts(res.data);
+            })
+            .catch((error) => {
+                console.log(error); // Gestione degli errori nella console
+            });
+    }
+}
